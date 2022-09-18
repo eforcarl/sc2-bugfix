6129b09896a0cd1896d06204d6be31e125a04fc9
eforcarl <107192760+eforcarl@users.noreply.github.com>
2022-09-18 21:14:59 +0800

    Add files via upload

---
+++ fixing stupid sc2 bug/sc2fix/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+void lol()
+{
+    var fpath = Environment.CurrentDirectory + "\\" + "fix.bat";
+    Process proc = new Process();
+    proc.StartInfo.FileName = fpath;
+    proc.Start();
+    proc.WaitForExit();
+    while (true)
+    {
+        Process[] pname = Process.GetProcessesByName("SC2_x64");
+        if (pname.Length == 0)
+        {
+            Console.WriteLine("process killed");
+            loop();
+        }
+        else
+        {
+            Console.WriteLine("still running");
+        }
+        Task.Delay(1000).Wait();
+    }
+}
+
+void loop()
+{
+    while (true)
+    {
+        Process[] pname = Process.GetProcessesByName("SC2_x64");
+        if (pname.Length == 0)
+        {
+            Console.WriteLine("no proc");
+        }
+        else
+        {
+            Console.WriteLine("proc found");
+            Task.Delay(10000).Wait();
+            lol();
+            break;
+        }
+        Task.Delay(1000).Wait();
+    }
+
+}
+
+loop();
---
+++ fixing stupid sc2 bug/sc2fix/bin/Release/net6.0/fix.bat
@@ -0,0 +1,2 @@
+ipconfig /release & ping -n 2 127.0.0.1 & ipconfig /renew
+exit
